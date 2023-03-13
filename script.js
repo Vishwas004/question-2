3c9244b675f7f0bf24599ae1144266fd9d62dc87
Vishwas004 <119989647+Vishwas004@users.noreply.github.com>
2023-03-13 19:33:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function store(){
     let Payment=document.getElementById("ptype").value;
     let Age=document.getElementById("age").value;
     let Upload=document.getElementById("photo").value;
-    
 
     let name=localStorage.setItem("Name",Name);
     let email=localStorage.setItem("Email",Email);
@@ -20,29 +19,44 @@ function store(){
     let age=localStorage.setItem("Age",Age);
     let upload=localStorage.setItem("Upload",Upload);
 }
-
-
-
 function validate(){ 
-    var regName = " ";
-    var name = document.getElementById('fname').value;
-    if(!regName.test(name)){
-        // alert('Please enter your full name (first & last name).');
-        document.getElementById('fname').focus();
-        return false;
-    }else{
-        alert('Valid name given.');
-        return true;
-    }
+    let Name=document.getElementById("fname").value;
+    let Phone=document.getElementById("phone").value;
+    let Age=document.getElementById("age").value;
 
-    var regPhon = /^(\d{3})[- ]?(\d{3})[- ]?(\d{4})$/;
-    var Phone = document.getElementById('phone').value;
-    if(!regPhon.test(Phone)){
-        alert('Please enter your current number');
-        document.getElementById('phone').focus();
+    if (Name == "") {
+        alert("plz...enter tha name");
         return false;
-    }else{
-        alert('Valid number given.');
-        return true;
     }
+    if (!isNaN(Name)) {
+        alert("plz...enter tha name");
+        return false;
+    }
+    if (Age < 10) {
+        alert("your age is very small");
+        return false; 
+    } 
+    if (isNaN(Phone)) {
+        alert("plz...Enter Only Number");
+        return false; 
+    } 
+    
+    return true;
 }
+// function add(){
+//     if(validate() == true){
+//     let Name=document.getElementById("fname").value;
+//     let Email=document.getElementById("mail").value;
+//     let Phone=document.getElementById("phone").value;
+//     let Birthdate=document.getElementById("dob").value;
+//     let Time=document.getElementById("time").value;
+//     let Color=document.getElementById("color").value;
+//     let Payment=document.getElementById("ptype").value;
+//     let Age=document.getElementById("age").value;
+//     let Upload=document.getElementById("photo").value;  
+        
+//     Name.push({
+//         name:name;
+//     })
+// }
+// }
