ba5225b32d1dab1ac9db8b2896cceb477e9bc898
Vishwas004 <119989647+Vishwas004@users.noreply.github.com>
2023-03-14 16:33:46 +0530

    git
    done

--- script.js
+++ script.js
@@ -43,20 +43,3 @@ function validate(){
     
     return true;
 }
-// function add(){
-//     if(validate() == true){
-//     let Name=document.getElementById("fname").value;
-//     let Email=document.getElementById("mail").value;
-//     let Phone=document.getElementById("phone").value;
-//     let Birthdate=document.getElementById("dob").value;
-//     let Time=document.getElementById("time").value;
-//     let Color=document.getElementById("color").value;
-//     let Payment=document.getElementById("ptype").value;
-//     let Age=document.getElementById("age").value;
-//     let Upload=document.getElementById("photo").value;  
-        
-//     Name.push({
-//         name:name;
-//     })
-// }
-// }
